b2d60e1b2e1fc1bbd0c79ffe0e415ce64faeb5a5
Daniel Choi <dhchoi@gmail.com>
2011-08-19 21:02:41 -0400

    notes

--- db/create.sql
+++ db/create.sql
@@ -5,6 +5,7 @@ create table tasks (
   context varchar,
   priority varchar, /* maybe change to enum */
   parent_id integer, /* add foreign key constraint; use for recursive queries */
+  position integer,
   created_at timestamp default now(),
   due_at timestamp, /* add this */
   completed_at timestamp
