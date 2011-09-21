1bd86aa085af08f7c5317124ab815c6728b172b4
Daniel Choi <dhchoi@gmail.com>
2011-09-21 06:54:08 -0400

    query as postgres -f file

---
+++ query.sql
@@ -0,0 +1,8 @@
+with recursive subtasks(title, parent_id, depth, position, path, task_id) as (
+  select title, parent_id, 0, position, array[position], task_id from tasks where parent_id is null
+union all
+  select
+    lpad('', (depth+1)*2 , '  ') || t.title, t.parent_id, depth+1, t.position, path || t.position, t.task_id
+  from tasks t, subtasks s
+  where s.task_id  = t.parent_id
+) select * from subtasks order by path;
