9fde28e7047bc99a114b193cd8751ede1292f58b
Daniel Choi <dhchoi@gmail.com>
2011-10-12 14:40:53 -0400

    format sequel output

--- lib/outln.rb
+++ lib/outln.rb
@@ -1,6 +1,32 @@
 require 'sequel'
 
 DB = Sequel.connect 'postgres:///tracker'
+
+FIELDS = { 
+  title: "%-72s",
+  parent_id: "%9s",
+  depth: "%6s",
+  position: "%9s",
+  path: "%-30s",
+  task_id: "%9s",
+}
+
+def format(result_set)
+  template = FIELDS.values.join(" | ")
+  header = template % FIELDS.keys
+  divider_line = header.gsub(/\|/, "+").gsub(/[^+]/, '-')
+  output = result_set.inject([header, divider_line]) do |memo, row|
+    vals = row.keys.inject([]) {|s, key| 
+      val = row[key]
+      colwidth = FIELDS[key][/\d+/,0].to_i - 1
+      s << val.to_s[0, colwidth]
+    }
+    line = template % vals
+    memo << line
+  end
+  output.map {|line| "  #{line}" }.join("\n")
+end
+
 def show
   query = <<SQL
 with recursive subtasks(title, parent_id, depth, position, path, task_id) as (
@@ -12,7 +38,7 @@ union all
   where s.task_id  = t.parent_id
 ) select * from subtasks order by path;
 SQL
-  DB[query].to_a
+  format DB[query]
 end
 
 if __FILE__ == $0
--- lib/outln.vim
+++ lib/outln.vim
@@ -1,4 +1,3 @@
 let s:client_cmd = "ruby -I lib/outln.rb "
 
-
 " show the current outln
