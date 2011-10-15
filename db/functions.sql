bd4e3ecc77acd490dd39080610d049aef11ac481
Daniel Choi <dhchoi@gmail.com>
2011-10-15 17:56:19 -0400

    use a view for the outline

--- db/functions.sql
+++ db/functions.sql
@@ -1,4 +1,3 @@
-
 CREATE OR REPLACE FUNCTION remove_task(integer) RETURNS VOID AS $$
 BEGIN
   update tasks set position = position - 1
@@ -8,17 +7,24 @@ BEGIN
   RETURN;
 END $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION insert_task(integer, integer, integer) RETURNS VOID AS $$
-DECLARE
-  target_parent ALIAS FOR $2;
-  target_position ALIAS FOR $3;
+CREATE OR REPLACE FUNCTION insert_task(integer, target_parent integer, target_position integer) RETURNS VOID AS $$
 BEGIN
-  update tasks set position = position + 1
-    where parent_id = target_parent and position >= target_position;
-  update tasks set position = target_position, parent_id = target_parent
-    where task_id = $1;
+  update tasks set position = position + 1 where parent_id = target_parent and position >= target_position;
+  /* NOTE. Data integrity will be lost if this is called before removing
+  the task from the old place in the three. Can just put an IF
+  condition here to check that the inserted task has null parent_id and
+  position */
+  update tasks set position = target_position, parent_id = target_parent where task_id = $1;
   RETURN;
 END $$ LANGUAGE plpgsql;
 
-
+CREATE OR REPLACE VIEW outline AS 
+  WITH recursive subtasks(title, parent_id, depth, position, path, task_id) as (
+    select title, 0, 0, position, array[position], task_id from tasks where parent_id is null
+  union all
+    select
+      lpad('', (depth+1)*2 , '  ') || t.title, t.parent_id, depth+1, t.position, path || t.position, t.task_id
+    from tasks t, subtasks s
+    where s.task_id  = t.parent_id
+  ) select * from subtasks order by path;
 
--- lib/outln.rb
+++ lib/outln.rb
@@ -28,21 +28,11 @@ def format(result_set)
 end
 
 def show
-  query = <<SQL
-with recursive subtasks(title, parent_id, depth, position, path, task_id) as (
-  select title, parent_id, 0, position, array[position], task_id from tasks where parent_id is null
-union all
-  select
-    lpad('', (depth+1)*2 , '  ') || t.title, t.parent_id, depth+1, t.position, path || t.position, t.task_id
-  from tasks t, subtasks s
-  where s.task_id  = t.parent_id
-) select * from subtasks order by path;
-SQL
+  query = "select * from outline"
   format DB[query]
 end
 
 if __FILE__ == $0
-
   puts show
 end
 
