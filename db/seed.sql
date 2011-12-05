12e1ee4a54ab6ccc8765446768b8f9e0edfb7ed9
Daniel Choi <dhchoi@gmail.com>
2011-12-05 08:57:12 -0500

    add some seed data

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+
+insert into tasks (title, parent_id, position) values ('Task A', 0, 0);
+insert into tasks (title, parent_id, position) values ('Task A1', 1, 0);
+insert into tasks (title, parent_id, position) values ('Task A2', 1, 1);
+insert into tasks (title, parent_id, position) values ('Task B', 0, 1);
+insert into tasks (title, parent_id, position) values ('Task C', 0, 2);
